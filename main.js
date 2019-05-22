a88aea2b78687a3ad365dbc7414b26799e08a7d9
a7madd94 <44666121+a7madd94@users.noreply.github.com>
2019-05-22 05:05:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function goToLink(){
-  window.location.href = 'http://khripankov.xyz/WmrTwx5P';
+  window.location.href = 'https://t.hrtyi.com/s21zdvwwg0?url_id=0&aff_id=34028&offer_id=3785&source=Instagram&nopop=1';
 }
